054090dde06f2d6bc8dd34fdddc6749d24125dde
Saul Burgos <saulburgos6@gmail.com>
2023-04-28 22:15:32 -0400

    add migration

--- cmd/api/boostrap/boostrap.go
+++ cmd/api/boostrap/boostrap.go
@@ -9,6 +9,11 @@ import (
 	"github.com/osmait/blog-go/internal/find"
 	"github.com/osmait/blog-go/internal/platfrom/server"
 	"github.com/osmait/blog-go/internal/platfrom/storage/postgres"
+	"github.com/rs/zerolog/log"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 const (
@@ -28,6 +33,8 @@ func Run() error {
 	if err != nil {
 		return err
 	}
+	runDBMigration("file://db/migration", postgresURI)
+
 	userRepository := postgres.NewUserRepository(db)
 	postRepository := postgres.NewPostRepository(db)
 	creatingUserService := creating.NewCreateUseService(userRepository)
@@ -37,4 +44,18 @@ func Run() error {
 	srv := server.NewServer(host, port, *creatingUserService, *creatingPostService, *findUser)
 	return srv.Run()
 
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	fmt.Println(migrationURL)
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal().Err(err).Msg("cannot create new migrate instance")
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal().Err(err).Msg("failed to run migrate up")
+	}
+
+	log.Info().Msg("db migrated successfully")
 }
